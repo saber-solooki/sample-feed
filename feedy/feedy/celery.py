c92db58d797be497c4985a75f446acd9f6e57c4b
sabx13 <saber.solooki@gmail.com>
2020-08-22 19:38:15 +0430

    fix celery setting for docker

--- feedy/feedy/celery.py
+++ feedy/feedy/celery.py
@@ -3,8 +3,8 @@ from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'feedy.settings')
 
-# app = Celery('feedy_tasks', broker=os.environ['CELERY_BROKER_URL'], backend=os.environ['CELERY_BACKEND_URL'])
-app = Celery('feedy_tasks')
+app = Celery('feedy_tasks', broker=os.environ['CELERY_BROKER_URL'], backend=os.environ['CELERY_BACKEND_URL'])
+# app = Celery('feedy_tasks')
 
 app.config_from_object('feedy.celerysetting', namespace='CELERY')
 
